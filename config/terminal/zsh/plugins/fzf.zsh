0d3550e7e83033fbcf22ea6a683a369175a2ce3d
Joel Eapen <joelpeapen@protonmail.com>
2024-06-11 14:51:22 +0530

    feat(fzf): update global, spotlight, lf fzf previews

--- config/terminal/zsh/plugins/fzf.zsh
+++ config/terminal/zsh/plugins/fzf.zsh
@@ -3,14 +3,18 @@
 export FZF_DEFAULT_COMMAND="fd -t d -LH"
 export FZF_DEFAULT_OPTS="--preview \
 'case \$(file --mime-type -Lb {}) in
-    image/*) kitty icat --clear --transfer-mode stream --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {};;
-    video/*) vidthumb {} | kitty icat --clear --transfer-mode file --stdin yes --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0;;
-    text/csv) csview {};;
+    *csv) csview {};;
+    *text* | *json | *xml) bat -n --color=always {};;
+    audio*) mediainfo {};;
+    image*) kitty icat --clear --transfer-mode stream --stdin no --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {};;
+    video*) kitty icat --clear --transfer-mode stream --stdin no --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 \$(vidthumb {});;
+    *pdf) pdftoppm -jpeg -f 1 -singlefile "\$1" | kitten icat --silent --stdin yes --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0;;
     inode/directory) eza -l --icons --no-user --no-time --color=always {} | bat -n;;
-    *) bat -n --color=always {};;
+    *) pistol {};;
 esac' \
 --multi
 --reverse
+--preview-window hidden
 --bind=change:first,\
 tab:down,btab:up,\
 alt-i:select,\
