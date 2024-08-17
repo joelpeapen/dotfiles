50f1d73963b20348954730e1ae4f0ea760c0fbe5
Joel Eapen <joelpeapen@protonmail.com>
2024-08-17 19:18:48 +0530

    refactor(nvim): lualine

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -1,18 +1,4 @@
-local function lsp()
-    local msg = ''
-    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then return msg end
-    for _, client in ipairs(clients) do
-        local filetypes = client.config.filetypes
-        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-            return client.name
-        end
-    end
-    return msg
-end
-
-local function diff_source()
+local function diff()
     local gitsigns = vim.b.gitsigns_status_dict
     if gitsigns then
         return {
@@ -23,6 +9,18 @@ local function diff_source()
     end
 end
 
+local function lsp()
+    local clients = vim.lsp.get_clients()
+    if next(clients) == nil then return "" end
+    for _, client in ipairs(clients) do
+        local filetypes = client.config.filetypes
+        if filetypes and vim.fn.index(filetypes, vim.bo.filetype) ~= -1 then
+            return client.name
+        end
+    end
+    return ""
+end
+
 require("lualine").setup({
     theme = "auto",
     options = {
@@ -31,36 +29,36 @@ require("lualine").setup({
         disabled_filetypes = { "startify" }
     },
     sections = {
-        lualine_a = { { "filename", color = { gui = "bold" } } },
+        lualine_a = {
+            { "filename", color = { gui = "bold" } },
+        },
         lualine_b = {
             { "branch", color = { fg = "#c099ff" } },
-            { "diff",   source = diff_source },
-            "diagnostics"
+            { "diff",   source = diff },
+            "diagnostics",
+            "filesize"
         },
-        lualine_c = { nil },
+        lualine_c = { },
         lualine_x = {
             { lsp, icon = '󰒓', color = { fg = '#f7768e' } },
-            { "filesize" },
             { "filetype", color = { fg = "#7e9cd8" } }
         },
-        lualine_z = { { "location", color = { gui = "bold" } } }
+        lualine_z = {
+            { "location", color = { gui = "bold" } },
+        }
     },
     inactive_sections = {
-        lualine_a = { "filename" },
         lualine_b = {
             { "branch", color = { fg = "#c099ff" } },
-            { "diff",   source = diff_source },
-            "diagnostics"
+            { "diff",   source = diff },
+            "diagnostics",
         },
-        lualine_c = { nil },
         lualine_x = {
-            { "filesize" },
             { "filetype", color = { fg = "#7e9cd8" } }
         }
     },
     extensions = {
-        "nvim-tree", "nvim-dap-ui",
-        "fugitive", "lazy", "toggleterm",
-        "trouble", "quickfix", "man"
+        "nvim-dap-ui", "fugitive", "lazy",
+        "toggleterm", "trouble", "quickfix", "man",
     }
 })
