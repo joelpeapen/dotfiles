05cb40b9700e94e5f430d69efdaec9dd4e1d2c17
Joel Eapen <joelpeapen@protonmail.com>
2024-09-11 16:15:49 +0530

    feat(fzf): add pdf preview (still broken in lf)

--- config/zsh/plugins/fzf.zsh
+++ config/zsh/plugins/fzf.zsh
@@ -4,7 +4,6 @@ export FZF_DEFAULT_COMMAND="fd -t d -LH"
 export FZF_DEFAULT_OPTS="
 --multi
 --reverse
---preview-window hidden
 --bind=change:first,\
 tab:down,\
 btab:up,\
@@ -23,17 +22,18 @@ alt-j:half-page-down,\
 ctrl-s:toggle-sort,\
 alt-u:preview-up,\
 alt-d:preview-down,\
-ctrl-space:toggle-preview \
+ctrl-space:toggle-preview
 --preview \
 'case \$(file --mime-type -Lb {}) in
     *csv) csview {};;
     *text* | *json | *xml) bat -n --color=always {};;
     audio*) mediainfo {};;
-    image*) kitty icat --clear --transfer-mode stream --stdin no --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {};;
-    video*) kitty icat --clear --transfer-mode stream --stdin no --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 \$(vidthumb {});;
-    *pdf) pdftoppm -jpeg -f 1 -singlefile "\$1" | kitten icat --silent --stdin yes --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0;;
+    image*) kitty icat --stdin no --clear --transfer-mode stream --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {};;
+    video*) kitty icat --stdin no --clear --transfer-mode stream --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 \$(vidthumb {});;
+    *pdf) kitty icat --stdin yes --clear --transfer-mode stream --place \${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 \
+          <<< \$(pdftoppm -jpeg -f 1 -l 1 -tiffcompression jpeg -scale-to-x -1 -scale-to-y 1080 -singlefile {});;
     inode/directory) eza -l --icons --no-user --no-time --color=always {} | bat -n;;
-    *) pistol {};;
+    *) pistol {} | bat -n;;
 esac'"
 
 export FZF_CTRL_T_COMMAND="fd -t f -LH"
