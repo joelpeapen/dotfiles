e426877cbf807c30443dc780f11e3456948d0442
Joel Eapen <joelpeapen@protonmail.com>
2024-09-21 15:17:48 +0530

    refactor(nvim): move completion to separate file

---
+++ config/nvim/after/plugin/completion.lua
@@ -0,0 +1,50 @@
+local cmp = require("cmp")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end
+    },
+    sources = cmp.config.sources {
+        { name = "path" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "nvim_lsp" }
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<c-Space>"] = cmp.mapping.complete(),
+        ["<m-;>"] = cmp.mapping.select_next_item(),
+        ["<m-'>"] = cmp.mapping.select_prev_item(),
+        ["<m-Space>"] = cmp.mapping.confirm({ select = true }),
+        ["<m-d>"] = cmp.mapping.scroll_docs(4),
+        ["<m-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<m-o>"] = function()
+            if cmp.visible_docs() then
+                cmp.close_docs()
+            else
+                cmp.open_docs()
+            end
+        end
+    })
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = ({
+        { name = "path" },
+        { name = "cmdline" }
+    })
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = ({
+        { name = "buffer" },
+    })
+})
--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,3 @@
-local cmp = require("cmp")
 local lsp = require("lspconfig")
 
 require("mason").setup()
@@ -37,53 +36,6 @@ require("mason-lspconfig").setup({
             })
         end
     }
-})
-
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end
-    },
-    sources = cmp.config.sources {
-        { name = "path" },
-        { name = "buffer" },
-        { name = "luasnip" },
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" }
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<c-Space>"] = cmp.mapping.complete(),
-        ["<m-;>"] = cmp.mapping.select_next_item(),
-        ["<m-'>"] = cmp.mapping.select_prev_item(),
-        ["<m-Space>"] = cmp.mapping.confirm({ select = true }),
-        ["<m-d>"] = cmp.mapping.scroll_docs(4),
-        ["<m-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<m-o>"] = function()
-            if cmp.visible_docs() then
-                cmp.close_docs()
-            else
-                cmp.open_docs()
-            end
-        end
-    })
-})
-
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = ({
-        { name = "path" },
-        { name = "cmdline" }
-    })
-})
-
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = ({ { name = "buffer" } })
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
